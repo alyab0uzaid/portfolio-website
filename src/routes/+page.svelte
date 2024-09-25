35a27fa3886bfb7709c20391ae4c1dde77d1a3e9
alyab0uzaid <aabouza@siue.edu>
2024-09-24 22:54:59 -0500

    add navbar

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="navbar bg-base-100 mx-auto px-4">
+  <div class="flex-1">
+    <a class="btn btn-ghost normal-case text-xl">Aly Abou-Zaid</a>
+  </div>
+  <div class="flex-none">
+    <ul class="menu menu-horizontal px-1">
+      <li><a>Home</a></li>
+      <li><a>Projects</a></li>
+      <li><a>Contact</a></li>
+    </ul>
+  </div>
+</div>
