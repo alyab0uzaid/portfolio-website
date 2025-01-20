fc8b5fe238d3c9b0169c282c711e2a38ccc340e8
alyab0uzaid <aabouza@siue.edu>
2025-01-20 14:50:29 -0600

    fixed sunset pictures

--- src/routes/sunsets/+page.svelte
+++ src/routes/sunsets/+page.svelte
@@ -1,10 +1,10 @@
 <script>
     const sunsetPics = [
-      { src: "/images/sunset1.jpg", alt: "Sunset 1", colSpan: "col-span-2", rowSpan: "row-span-1" },
-      { src: "static/images/sunset2.jpg", alt: "Sunset 2", colSpan: "col-span-1", rowSpan: "row-span-1" },
-      { src: "static/images/sunsets/sunset5.jpg", alt: "Sunset 5", colSpan: "col-span-1", rowSpan: "row-span-1" },
-      { src: "static/images/sunsets/sunset3.jpg", alt: "Sunset 3", colSpan: "col-span-2", rowSpan: "row-span-2" },
-      { src: "static/images/sunsets/sunset4.jpg", alt: "Sunset 4", colSpan: "col-span-1", rowSpan: "row-span-1" },
+      { src: "/images/sunsets/sunset1.jpg", alt: "Sunset 1", colSpan: "col-span-2", rowSpan: "row-span-1" },
+      { src: "/images/sunsets/sunset2.jpg", alt: "Sunset 2", colSpan: "col-span-1", rowSpan: "row-span-1" },
+      { src: "/images/sunsets/sunset5.jpg", alt: "Sunset 5", colSpan: "col-span-1", rowSpan: "row-span-1" },
+      { src: "/images/sunsets/sunset3.jpg", alt: "Sunset 3", colSpan: "col-span-2", rowSpan: "row-span-2" },
+      { src: "/images/sunsets/sunset4.jpg", alt: "Sunset 4", colSpan: "col-span-1", rowSpan: "row-span-1" },
 
     ];
 
