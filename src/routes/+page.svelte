3f19f9d9c0e1be91fbd18395a9b2173d1b42ccf3
alyab0uzaid <aabouza@siue.edu>
2025-01-25 22:53:05 -0600

    fix link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -41,7 +41,7 @@
 		  "An interactive website that maps endangered species at the Saint Louis Zoo.",
 		image: "/images/zooss.png",
 		badges: ["HTML", "CSS", "Leaflet.js"],
-		website: "stlzoovisualization.pages.dev",
+		website: "https://stlzoovisualization.pages.dev",
 		showWebsite: true,
 		source: "#",
 		showSource: true,
